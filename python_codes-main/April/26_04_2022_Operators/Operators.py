9591147c219dc66ee9fb86c76204591a8e9cf1c3
Saurabh Varule <varulesaurabh44@gmail.com>
2025-08-18 16:28:17 +0530

    add statements

--- python_codes-main/April/26_04_2022_Operators/Operators.py
+++ python_codes-main/April/26_04_2022_Operators/Operators.py
@@ -4,45 +4,45 @@ y = 3
 
 # Arithmetic Operators
 
-print(x + y)
-print(x - y)
-print(x * y)
-print(x % y)
-print(x / y)
-print(x ** y)       #Index
-print(x // y)       #flooral division
+print("x + y = ",x + y)
+print("x - y = ",x - y)
+print("x * y = ",x * y)
+print("x % y = ",x % y)
+print("x / y = ",x / y)
+print("x ** y = ",x ** y)       #Index
+print("x // y = ",x // y)       #flooral division
 
 # Assignment Operators
 
 x += y
-print(x)
+print("x = ",x)
 
 x -= y
-print(x)
+print("x = ",x)
 
 x *= y
-print(x)
+print("x = ",x)
 
 x /= y
-print(x)
+print("x = ",x)
 
 # Relational Operators - Returns boolean value.
 
-print(x < y)
-print(x <= y)
-print(x > y)
-print(x >= y)
-print(x == y)
-print(x != y)
+print("x < y = ",x < y)
+print("x <= y = ",x <= y)
+print("x > y = ",x > y)
+print("x >= y = ",x >= y)
+print("x == y = ",x == y)
+print("x != y = ",x != y)
 
 # Logical Operator
 
-print(x and y)
-print(x or y)
+print("x and y = ",x and y)
+print("x or y = ",x or y)
 
 # Bitwise Operators
 
 x = 12
 y = 8
 
-print(x & y)
+print("x&y = ",x & y)
